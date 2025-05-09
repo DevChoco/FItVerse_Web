ecaa37e790701aa0e51efd729b393d6c75ec56c6
kimsehyun-34 <about7086@gmail.com>
2025-05-09 20:39:23 +0900

    2025.5.9_fix

--- my_website/script.js
+++
@@ -1,8 +0,0 @@
-function openModal(type) {
-  document.getElementById("modal").classList.remove("hidden");
-  document.getElementById("modal-title").innerText = type === "signup" ? "Sign Up" : "Log In";
-}
-
-function closeModal() {
-  document.getElementById("modal").classList.add("hidden");
-}
